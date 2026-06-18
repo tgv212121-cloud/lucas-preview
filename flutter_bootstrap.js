5ddc0fb90bb3448c8f32d538876d1c1a28f2d5a6
tgv212121-cloud <tgv212121@gmail.com>
2026-06-18 19:29:11 +0200

    Maj apercu: accueil + vue admin

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3606141858" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3071110672" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
