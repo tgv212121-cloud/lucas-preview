3e6b7b56c006cb800e97b747237a324b74a49b46
tgv212121-cloud <tgv212121@gmail.com>
2026-06-18 21:54:25 +0200

    Maj apercu: connexion, admin responsive, boutons appeler/itineraire

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4034716978" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4267672874" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
